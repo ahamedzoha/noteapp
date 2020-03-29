b16aee331b0c2dc9396d01e44c6484107fcd45cd
Azaz Ahamed <ahamed.zoha@gmail.com>
2020-03-29 22:45:13 +0600

    added select box for categories and update states accordingly

--- src/Pages/add-note/add-note.page.jsx
+++ src/Pages/add-note/add-note.page.jsx
@@ -6,11 +6,28 @@ export default class AddNotePage extends React.Component {
     super()
 
     this.state = {
-      category: "",
-      id: "",
+      category: "Home",
       title: "",
       body: ""
     }
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    this.setState({
+      category: "",
+      title: "",
+      body: ""
+    })
   }
 
   render() {
@@ -18,9 +35,34 @@ export default class AddNotePage extends React.Component {
       <div className="page-container">
         <h1>Add your Notes</h1>
 
-        <form action="">
-          <input type="text" />
-          <input type="text" />
+        <form className="form-wrapper" onSubmit={this.handleSubmit}>
+          <label>Select Category</label>
+
+          <select onChange={this.handleChange} name="category">
+            <option value="Home">Home</option>
+            <option value="School">School</option>
+            <option value="Office">Office</option>
+            <option value="Other">Other</option>
+          </select>
+
+          <label>Add Title...</label>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+            required
+          />
+          <label>Note Description</label>
+          <input
+            type="text"
+            name="body"
+            value={this.state.body}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">Add Note</button>
         </form>
       </div>
     )
